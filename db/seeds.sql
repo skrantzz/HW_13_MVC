c77d7e9dfc7bcbbf5fecba0e73c321d3e656394e
Sydney Krantz <syd107@gmail.com>
2020-04-02 12:47:40 -0700

    first commit setting up files and mysql table done

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheddar Ostrich Burger");
+INSERT INTO burgers (burger_name)
+VALUES ("BBQ Salsa Goat Burger");
+INSERT INTO burgers (burger_name)
+VALUES ("Chili Onion Buffalo Burger");
