79c9cf1dccfc0dec17aeddc98d5cbf5e5c1072bd
Sydney Krantz <syd107@gmail.com>
2020-04-03 22:56:47 -0700

    front end kind of working

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+// 2nd
+var orm = require("../config/orm.js");
+
+
+var burger = {
+    all: function(cb) {
+      orm.all("burgers", function(data) {
+        cb(data);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller (catsController.js).
+  module.exports = burger;
+  
--- server.js
+++ server.js
@@ -3,22 +3,22 @@ var express = require("express");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+var exphbs = require("express-handlebars");
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burger_controller");
 
 app.use(routes);
 
