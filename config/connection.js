9a533518af92d224148eb1f20538c11f7f9bc181
Sydney Krantz <syd107@gmail.com>
2020-04-04 18:00:02 -0700

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ var connection;
       connection = mysql.createConnection(process.env.JAWSDB_URL);
   } else {
     connection = mysql.createConnection({
-        host: "us-cdbr-iron-east-01.cleardb.net",
+        host: "localhost",
         port: 3306,
-        user: "b4edf71c1f2ffb",
-        password: "e6466a5f",
+        user: "root",
+        password: "password",
         database: "burgers_db"
       });
   }
